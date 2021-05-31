2b483f7c6748859fc028aaa687334b3f5b30bc83
Aaron Au Yoong <auyoong.aaron@gmail.com>
2021-05-31 15:56:57 +0800

    expanded root directory. managed to get initial express server running.

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
-DB_NAME=
-DB_PASSWORD=rootPassword
-DB_USER=root
+const express = require('express');
+const session = require('express-session');
+const path = require('path');
+const app = express();
+const sequelize = require('./config/connection');
+
+const PORT = process.env.PORT || 3001;
+
+// Set up Handlebars.js engine with custom helpers
+
+// Inform Express.js on which template engine to use
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Now listening on PORT:${PORT}!`));
+});
