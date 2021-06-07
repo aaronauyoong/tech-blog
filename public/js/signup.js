ba7a69e17d9e1ce82a19590b66bf002a2e29e502
Aaron Au Yoong <auyoong.aaron@gmail.com>
2021-06-07 23:02:11 +0800

    fixed styles for login and signup form, added pseudo code for signup.js frontend form

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,26 +1,26 @@
-// // This is for signing up new users
-// const signupForm = async (event) => {
-//     event.preventDefault();
+// This is for signing up new users
+const signupForm = async (event) => {
+    event.preventDefault();
 
-//     const username = document.querySelector('#username-signup').value.trim();
-//     const email = document.querySelector('#email-signup').value.trim();
-//     const password = document.querySelector('#password-signup').value.trim();
+    const username = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
 
-//     if (username && email && password) {
-//         const response = await fetch('/api/users', {
-//             method: 'Post',
-//             body: JSON.stringify({ username, email, password }),
-//             headers: { 'Content-Type': 'application/json' },
-//         });
+    if (username && email && password) {
+        const response = await fetch('/api/users', {
+            method: 'Post',
+            body: JSON.stringify({ username, email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
 
-//         if (response.ok) {
-//             document.location.replace('/dashboard');
-//         } else {
-//             alert(response.statusText);
-//         }
-//     }
-// }
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
 
-// document
-//     .querySelector('.signup-form')
-//     .addEventListener('submit', signupForm);
+document
+    .querySelector('.signup-form')
+    .addEventListener('submit', signupForm);
