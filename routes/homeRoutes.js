b0451ae9d19de65a6062cb9343cb08f499f9c5ce
Aaron Au Yoong <auyoong.aaron@gmail.com>
2021-06-14 00:24:36 +0800

    homepage with posts now displays all posts. However, the helper function format_date is not working now. Backend for adding new posts still WIP. Need to create a handlebar for viewing enlarged/expanded post with associated comments. Login still needs to be fixed.

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -4,7 +4,6 @@ const withAuth = require("../utils/auth");
 
 router.get('/', async (req, res) => {
 	try {
-	  // Get all projects and JOIN with user data
 	  const postData = await Post.findAll({
 		include: [
 		  {
@@ -18,7 +17,7 @@ router.get('/', async (req, res) => {
 	  const posts = postData.map((post) => post.get({ plain: true }));
   
 	  // Pass serialized data and session flag into template
-	  res.render("public", { 
+	  res.render("homepage", { 
 		posts, 
 		logged_in: req.session.logged_in 
 	  });
@@ -40,7 +39,7 @@ router.get('/', async (req, res) => {
 // 		});
 // 		const posts = postData.map((post) => post.get({ plain: true }));
 
-// 		res.render("public", {
+// 		res.render("homepage", {
 // 			posts,
 // 			logged_in: req.session.logged_in,
 // 		});
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
 const routes = require("./routes");
+const auth = require("./utils/auth")
 const dateHelper = require("./utils/dateHelper");
 
 const sequelize = require("./config/connection");
