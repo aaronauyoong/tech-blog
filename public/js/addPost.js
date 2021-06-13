9afbe2abeac662b9beda21dbd29a2473349b6d9e
Aaron Au Yoong <auyoong.aaron@gmail.com>
2021-06-14 00:01:23 +0800

    added hide class to hide/show sections of webpage

--- public/js/addPost.js
+++ public/js/addPost.js
@@ -1,28 +1,38 @@
 // Create new post (front-end)
-const createNewPost = async(event) => {
-    event.preventDefault();
 
-    const title = document.querySelector("#new-post-title");
-    const content = document.querySelector("#new-post-content")
+const addNewPostBtn = document.querySelector("#create-post-btn");
+const createPostCard = document.querySelector('#create-post-card');
 
-    if (title && content) {
-        const response = await fetch("/api/posts", {
-            method: "POST",
-            body: JSON.stringify({ title, content }),
-            headers: {
-                "Content-Type": "application/json",
-            },
-        });
 
-        if (response.ok) {
-            document.location.replace("/dashboard");
-        } else {
-            alert("Failed to add a new blog post.");
-        }
-    }
+const showForm = async (event) => {
+	event.preventDefault();
+    createPostCard.classList.remove('hide');
+    addNewPostBtn.classList.add('hide');
 };
 
-// Listener
-document
-    .querySelector('#new-post-form')
-    .addEventListener('click', createNewPost);
+const createNewPost = async (event) => {
+	event.preventDefault();
+
+	const title = document.querySelector("#new-post-title");
+	const content = document.querySelector("#new-post-content");
+
+	if (title && content) {
+		const response = await fetch("/api/posts", {
+			method: "POST",
+			body: JSON.stringify({ title, content }),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
+
+		if (response.ok) {
+			document.location.replace("/dashboard");
+		} else {
+			alert("Failed to add a new blog post.");
+		}
+	}
+};
+
+// Listeners
+document.querySelector("#new-post-form").addEventListener("click", createNewPost);
+addNewPostBtn.addEventListener("click", showForm);
