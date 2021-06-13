ea4416f2be93f8916ec9fadda39ffe4eba9b65ec
Aaron Au Yoong <auyoong.aaron@gmail.com>
2021-06-13 22:57:51 +0800

    added new dashboardRoutes.js file for routes when user is logged in (to C R U D their own posts)

---
+++ routes/dashboardRoutes.js
@@ -0,0 +1,13 @@
+// These are routes for dashboard - showing user's posts
+const router = require("express").Router();
+const { User, Comment, Post } = require("../models");
+
+// GET all posts from user
+
+// Create new post
+
+// Update existing post
+
+// Delete existing post
+
+module.exports = router;
