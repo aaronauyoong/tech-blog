415dfad32ecd119bedc8aec9bc08e2c7f0737521
Aaron Au Yoong <auyoong.aaron@gmail.com>
2021-06-13 23:22:20 +0800

    added frontend code for addPost (creating new post), adjusted handlebars for dashboard, adding comment and adding post. added pseudo code for addComment.js

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -3,11 +3,38 @@ const router = require("express").Router();
 const { User, Comment, Post } = require("../models");
 
 // GET all posts from user
+router.get("/", async (req, res) => {
+    if (!req.session.loggedIn) {
+        return res.redirect('/login')
+    } else {
+        res.render('addPost', {
+            logged_in: true
+        });
+    }
+})
 
 // Create new post
+router.get("/new", async (req, res) => {
+    if (!req.session.loggedIn) {
+        return res.redirect('/login')
+    }
+
+})
 
 // Update existing post
+router.post("/edit/:id", async (req, res) => {
+    if (!req.session.loggedIn) {
+        return res.redirect('/login')
+    }
+
+})
 
 // Delete existing post
+router.delete("/delete", async (req, res) => {
+    if (!req.session.loggedIn) {
+        return res.redirect('/login')
+    }
+
+})
 
 module.exports = router;
